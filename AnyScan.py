820ecd6397ac58632a6bd4e523efe84fdb844b37
Cryptph <39612908+Cryptph@users.noreply.github.com>
2018-12-09 23:33:27 +0700

    Add files via upload

--- AnyScan.py
+++ AnyScan.py
@@ -1,29 +1,39 @@
 import requests
+from collections import defaultdict
+from datetime import datetime
 
 address = "0xC3Dd90F7BD1cB523b4D6BC4bA81706e92F7Ff821"
-url = "http://api.etherscan.io/api?module=account&action=txlist&address=" + address + \
-      "&startblock=0&endblock=99999999&sort=asc&apikey=YourApiKeyToken"
 
-response = requests.get(url)
-content = response.json()
-result = content.get("result")
+####################################################################
+#Func: Get transaction list by address
+#Ret: Transaction list
+####################################################################
+def GetTxByAddress(address):
+      url = "http://api.etherscan.io/api?module=account&action=txlist&address=" + address + \
+            "&startblock=0&endblock=99999999&sort=asc&apikey=YourApiKeyToken"
 
-for n, tx in enumerate(result):
-      tx_timeStamp = tx.get("timeStamp")
-      tx_hash      = tx.get("hash")
-      tx_from      = tx.get("from")
-      tx_to        = tx.get("to")
-      tx_value     = tx.get("value")
+      response = requests.get(url)
+      content = response.json()
+      result = content.get("result")
 
-      print("Transaction: ", n)
-      print("timeStamp:\t", tx_timeStamp)
-      print("hash:\t\t", tx_hash)
-      print("from:\t\t", tx_from)
-      print("to:\t\t\t", tx_to)
-      print("value:\t\t", tx_value)
+      data = defaultdict(dict)
+      for n, tx in enumerate(result):
+            tx_timeStamp = int(tx.get("timeStamp"))
+            tx_hash      = tx.get("hash")
+            tx_from      = tx.get("from")
+            tx_to        = tx.get("to")
+            tx_value     = tx.get("value")
 
-      if tx_from == address:
-            print("sending")
-      else:
-            print("receiving")
-      print("\n")
+            data[n]["timeStamp"]    = datetime.utcfromtimestamp(tx_timeStamp).strftime('%Y-%m-%d %H:%M:%S')
+            data[n]["hash"]         = tx_hash
+            data[n]["from"]         = tx_from
+            data[n]["to"]           = tx_to
+            data[n]["value"]        = tx_value
+
+      return data
+####################################################################
+####################################################################
+
+txDataList = GetTxByAddress(address)
+#last transaction:
+print(txDataList[0])
