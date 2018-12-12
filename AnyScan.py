448b01b45cf025e2c540c193fcdeb870faa3077e
Cryptph <39612908+Cryptph@users.noreply.github.com>
2018-12-12 07:01:31 +0700

    Add telegram notification

--- AnyScan.py
+++ AnyScan.py
@@ -2,11 +2,24 @@ import requests
 from collections import defaultdict
 from datetime import datetime
 import threading
+import telegram
+
 
 address         = "0xC3Dd90F7BD1cB523b4D6BC4bA81706e92F7Ff821"
 address2        = "0x861c9b0ab53847ac8d9c27897824a2d36d298da3"
 contractQRX     = "0x1d5a98162a497cd204948bc5e00e98b44abc7cd4"
 contractTUSD    = "0x8dd5fbce2f6a956c3022ba3663759011dd51e73e"
+teleAnyScanBotToken = "620177198:AAGwyaIOPNl3nR29FI9n5a6EG6vS5ZTy8Ug"   #@AnyScanBotId
+ChatGroupId     = "-359727681"
+
+####################################################################
+#Func: Send message to telegram bot
+#Ret: Transaction list
+####################################################################
+def notifyTele(msg, chat_id, token):
+    url = "https://api.telegram.org/bot" + token +"/sendMessage?chat_id=" + chat_id + "&text=" + msg
+    response = requests.get(url)
+    print(response)
 
 ####################################################################
 #Func: Get transaction list by address
@@ -63,23 +76,24 @@ def GetTokenInfo(tokenContract):
     return content
 
 #Transaction count of last query
+lastTxCnt = 0
 
 ####################################################################
 #Func: Main function
 #Ret: nothing
 ####################################################################
-def Main():
-    lastTxCnt = 0
-    threading.Timer(5.0, Main).start()  # called every 5 seoconds
-
-    #Get token information
-    content = GetTokenInfo(contractTUSD)
+def ScanToken():
+    #"http://api.ethplorer.io/getTokenHistory/"
+    #"?apiKey=freekey&type=transfer&limit=5"
+    content = GetTokenInfo(contractQRX)
+    global lastTxCnt
     curTxCnt = int(content.get("transfersCount"))
     if curTxCnt > lastTxCnt:
+        notification = "QRX transaction count: " + str(curTxCnt) + ""
+        notifyTele(notification, ChatGroupId, teleAnyScanBotToken)
         print("Something new !! lastTxCnt: ",lastTxCnt, "curTxCnt: ", curTxCnt)
         lastTxCnt = curTxCnt
-        print("After show !! lastTxCnt: ", lastTxCnt, "curTxCnt: ", curTxCnt)
+    threading.Timer(60.0, ScanToken).start()  # called every 60 seconds
 
-Main()
-
+ScanToken()
 
